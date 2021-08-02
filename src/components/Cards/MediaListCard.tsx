d43c7ee269f577d29e07121d099e74cd05d116b1
Belendia Serda <belendia@gmail.com>
2021-08-02 23:34:03 +0300

    Change the MediaListCard from pure to normal component. Needs deep object comparision to solve the memorization of a wrong media object (server vs local)

--- src/components/Cards/MediaListCard.tsx
+++ src/components/Cards/MediaListCard.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -19,65 +19,62 @@ type MediaListCardProps = {
   onDeletePressed?: (media: Media) => void;
 };
 
-const MediaListCard = memo(
-  ({media, mediaSource, showDelete, onDeletePressed}: MediaListCardProps) => {
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
+const MediaListCard = ({
+  media,
+  mediaSource,
+  showDelete,
+  onDeletePressed,
+}: MediaListCardProps) => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-    return (
-      <View style={styles.cardContainer}>
-        <TouchableOpacity
-          activeOpacity={0.9}
-          onPress={() => {
-            dispatch(setMediaSlug(media.slug));
-            dispatch(dispatch(setMediaSource(mediaSource)));
-            navigation.navigate('MediaScreen', {
-              slug: media.slug,
-            });
-          }}>
-          <View style={styles.card}>
-            <Cover images={media?.images} />
-            <View style={styles.cardDetails}>
-              <Text style={styles.cardTitle} numberOfLines={3}>
-                {media.title}
-              </Text>
-              <Chip values={media.genres} />
-              <View style={styles.cardNumbers}>
-                <View style={styles.cardHeart}>
-                  <Ionicons name="heart" size={20} color={colors.red800} />
-                  <Text style={styles.cardRatings}>1.2k</Text>
-                </View>
+  return (
+    <View style={styles.cardContainer}>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={() => {
+          dispatch(setMediaSlug(media.slug));
+          dispatch(dispatch(setMediaSource(mediaSource)));
+          navigation.navigate('MediaScreen', {
+            slug: media.slug,
+          });
+        }}>
+        <View style={styles.card}>
+          <Cover images={media?.images} />
+          <View style={styles.cardDetails}>
+            <Text style={styles.cardTitle} numberOfLines={3}>
+              {media.title}
+            </Text>
+            <Chip values={media.genres} />
+            <View style={styles.cardNumbers}>
+              <View style={styles.cardHeart}>
+                <Ionicons name="heart" size={20} color={colors.red800} />
+                <Text style={styles.cardRatings}>1.2k</Text>
               </View>
-              <Text
-                style={styles.cardDescription}
-                numberOfLines={3}
-                ellipsizeMode="tail">
-                {media.description}
-              </Text>
             </View>
-            {showDelete && (
-              <TouchableOpacity
-                style={styles.remove}
-                onPress={() => onDeletePressed && onDeletePressed(media)}>
-                <MaterialIcons
-                  name={'delete-outline'}
-                  size={24}
-                  color={colors.red300}
-                />
-              </TouchableOpacity>
-            )}
+            <Text
+              style={styles.cardDescription}
+              numberOfLines={3}
+              ellipsizeMode="tail">
+              {media.description}
+            </Text>
           </View>
-        </TouchableOpacity>
-      </View>
-    );
-  },
-  (prevProps, nextProps) => {
-    if (prevProps.media.slug !== nextProps.media.slug) {
-      return false;
-    }
-    return true;
-  },
-);
+          {showDelete && (
+            <TouchableOpacity
+              style={styles.remove}
+              onPress={() => onDeletePressed && onDeletePressed(media)}>
+              <MaterialIcons
+                name={'delete-outline'}
+                size={24}
+                color={colors.red300}
+              />
+            </TouchableOpacity>
+          )}
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export {MediaListCard};
 
